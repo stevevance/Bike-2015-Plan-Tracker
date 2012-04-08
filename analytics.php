b35d5f0949367570ac183cc0ca60ab0d7fb7fef5
Steven Vance <steve@stevevance.net>
2012-04-07 23:02:30 -0500

    Updated analytics.php for public consumption

--- analytics.php
+++ analytics.php
@@ -1,14 +1,10 @@
 <?php 
 require_once("mysql.php");
-$host = $_SERVER['HTTP_HOST'];
-if(contains("stevevance.net",$host)) {
 ?>
-
 <script type="text/javascript">
 
   var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-7066585-1']);
-  _gaq.push(['_setDomainName', 'stevevance.net']);
+  _gaq.push(['_setAccount', '<?php echo $google_analytics; ?>']);
   _gaq.push(['_trackPageview']);
 
   (function() {
@@ -18,22 +14,3 @@ if(contains("stevevance.net",$host)) {
   })();
 
 </script>
-<?php
-} elseif(contains("gridchicago.com",$host)) {
-?>
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-24221135-3']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
-<?php
-}
-?>
